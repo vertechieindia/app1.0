6f0f947da17cccbdf9ccaafb76090de84ecf70d5
thenianandraj <thenianandraj@gmail.com>
2026-02-09 11:04:53 +0530

    config

--- vertechie_be/vertechie_fastapi/app/core/config.py
+++ vertechie_be/vertechie_fastapi/app/core/config.py
@@ -42,6 +42,7 @@ class Settings(BaseSettings):
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+         "https://vertechie.com",
     ]
     
     # Email - Gmail SMTP settings
